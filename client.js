72ea3141c5333f299a116d901c80b536210a0c4a
Robbie Thomas <robbiekt@gmail.com>
2023-01-17 13:56:06 -0800

    sent snake name to server

--- client.js
+++ client.js
@@ -11,7 +11,8 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log("Connected to Server");
+    console.log("Successfully connected to game server");
+    conn.write("Name: RKT");
   });
 
   conn.on("data", (data) => console.log(data));
