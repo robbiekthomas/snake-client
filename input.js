7bd297c27e2bf121b23520b85c0d4f94082c939f
Robbie Thomas <robbiekt@gmail.com>
2023-01-18 08:47:32 -0800

    added movement, messages, and a constants file

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +13,24 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "k") {
+    connection.write("Say: hi!");
+  }
+  if (key === "l") {
+    connection.write("Say: bye!");
   }
 };
 
